c8b7f066ec847399d6ea44dcc65338e96b234a52
Auto Logon <autologon@CSED.LAB>
2024-04-22 10:54:10 -0300

    VersaoL3

---
+++ src/br/edu/up/modelos/Exercicio2.java
@@ -0,0 +1,14 @@
+package br.edu.up.modelos;
+
+public class Exercicio2 {
+    public void executar() {
+
+        int Combustivel = 6;
+        int KmsRecorridos = 10;
+
+        System.out.println("A quantidade de Combustivel consumida foi: " + (KmsRecorridos / Combustivel));
+        System.out.println("A quantidade de Kilometros Recorridos foi: " + KmsRecorridos);
+        System.out.println("A quantidade de Combustivel que o carro tinha: " + Combustivel);
+
+    }
+}
