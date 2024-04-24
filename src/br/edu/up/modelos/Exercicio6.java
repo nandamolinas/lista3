eb6239121e857754c1bba5d641230988aaa65b72
nandamolinas <nandamoly04@gmail.com>
2024-04-24 16:42:22 -0300

    Exercicio6

---
+++ src/br/edu/up/modelos/Exercicio6.java
@@ -0,0 +1,23 @@
+package br.edu.up.modelos;
+
+import java.util.Scanner;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        try(Scanner Preco = new Scanner(System.in)){
+            
+            System.out.println("Digite o porcentagem que deseja que seja aplicado para o custo de venda(%): ");
+            double Porcentagem = Preco.nextInt();
+
+            System.out.println("Digite o Valor de costo do produto: ");
+            double Valor = Preco.nextInt();
+
+            double Venda = Valor / (100);
+
+            System.out.println("O Resultado de Custo de vendas sera: " + (Venda * Porcentagem));
+            
+        }
+
+       
+    }
+}
